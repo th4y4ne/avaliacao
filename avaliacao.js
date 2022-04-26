b55f9a3963608f6f831ac680d8a3b78b397cb84d
th4y4ne <101671179+th4y4ne@users.noreply.github.com>
2022-04-26 10:12:41 -0300

    Add files via upload

---
+++ avaliacao.js
@@ -0,0 +1,51 @@
+function getMovieInfosOMDB(title){
+    const url = `http://www.omdbapi.com/?t=${title}&apikey=790af7bc`  
+       
+fetch(url)
+    .then(response => response.json())
+    .then(data => {
+
+        if(data.Response == 'False'){
+            descriptionBodyNotFound.textContent = "Filme não encontrado! Tente novamente.";
+            descriptionBodyNotFound.style.marginBottom = '30px'
+            movieTitle.textContent = ""
+            movieYear.textContent = ""
+            movieGenre.textContent = ""
+            movieRuntime.textContent = ""
+            imdbRating.textContent = ""
+            movieInfo.textContent = ""
+            movieWriter.textContent = ""
+            movieDirector.textContent = ""
+            caixa1.textContent = ""
+            caixa2.textContent = ""
+            moviePoster.style.backgroundImage = `url(filmeNaoencontrado.png)`
+
+        } else {
+            descriptionBodyNotFound.textContent = "";
+            
+            movieTitle.textContent = data.Title
+            movieYear.textContent = data.Year
+            movieGenre.textContent = data.Genre
+            movieRuntime.textContent = data.Runtime
+            imdbRating.textContent = data.imdbRating
+            movieInfo.textContent = data.Plot
+            movieWriter.textContent = data.Writer
+            movieDirector.textContent = data.Director
+            moviePoster.style.backgroundImage = `url(${data.Poster})`
+        }
+        
+        
+     })  
+}
+
+var form = document.getElementById('formSearch');
+var title = document.getElementById('title');
+
+form.addEventListener('submit', function(e) {
+    
+    getMovieInfosOMDB(title.value)
+    // impede o envio do form
+    e.preventDefault();
+
+
+});
